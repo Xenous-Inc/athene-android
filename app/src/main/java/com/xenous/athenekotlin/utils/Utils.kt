fd5293f6f23d675d1d3f2a630beb0edac12cd6c4
x3noku <Artem.Kaikov@yandex.ru>
2020-06-30 15:47:48 +0600

    Utils Merge commit

--- app/src/main/java/com/xenous/athenekotlin/utils/Utils.kt
+++ app/src/main/java/com/xenous/athenekotlin/utils/Utils.kt
@@ -1,3 +1,16 @@
 package com.xenous.athenekotlin.utils
 
 fun isEmailValid(email: String): Boolean =  android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+const val SUCCESS_CODE = 9000
+const val ERROR_CODE = 9001
+
+const val USER_REFERENCE = "users"
+const val WORDS_REFERENCE = "words"
+const val CATEGORY_REFERENCE = "categories"
+
+const val NATIVE_WORD_DATABASE_KEY = "English"
+const val LEARNING_WORD_DATABASE_KEY = "Russian"
+const val WORD_CATEGORY_DATABASE_KEY = "category"
+const val WORD_LAST_DATE_DATABASE_KEY = "date"
+const val WORD_LEVEL_DATABASE_KEY = "level"
