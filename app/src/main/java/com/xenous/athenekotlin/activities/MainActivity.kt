3d06e19ea36b78ea767229c6c2a6bfedf38b9269
yourdefault <sheremetyev48@mail.ru>
2020-07-02 07:53:00 +0300

    Athene Dialog Commit -Добавлен класс Dialog для оповещения пользователя и общения с ним

--- app/src/main/java/com/xenous/athenekotlin/activities/MainActivity.kt
+++ app/src/main/java/com/xenous/athenekotlin/activities/MainActivity.kt
@@ -3,10 +3,14 @@ package com.xenous.athenekotlin.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.xenous.athenekotlin.R
+import com.xenous.athenekotlin.views.AtheneDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = AtheneDialog(this)
+
     }
 }
