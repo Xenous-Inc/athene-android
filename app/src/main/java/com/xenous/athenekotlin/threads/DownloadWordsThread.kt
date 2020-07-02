341630e228b8a6185162605f6f54a551c7eb54eb
yourdefault <sheremetyev48@mail.ru>
2020-07-02 09:32:10 +0300

    Add Word Thread Refactor Commit -Рефактор потока отправки слова на сервер

--- app/src/main/java/com/xenous/athenekotlin/threads/DownloadWordsThread.kt
+++ app/src/main/java/com/xenous/athenekotlin/threads/DownloadWordsThread.kt
@@ -52,7 +52,7 @@ class DownloadWordsThread(
                     val word = Word(
                         wordSnapshot.child(NATIVE_WORD_DATABASE_KEY).value as String?,
                         wordSnapshot.child(LEARNING_WORD_DATABASE_KEY).value as String?,
-                        wordSnapshot.child(CATEGORY_REFERENCE).value as String?,
+                        wordSnapshot.child(WORD_CATEGORY_DATABASE_KEY).value as String?,
                         wordSnapshot.child(WORD_LAST_DATE_DATABASE_KEY).value as Long?,
                         wordSnapshot.child(WORD_LEVEL_DATABASE_KEY).value as Long?,
                         wordSnapshot.key as String?
